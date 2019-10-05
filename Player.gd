98e878b0f187ed81077d64bda02b749aaec6a1ad
Gedas <g.simaitis@gmail.com>
2019-10-05 15:37:27 +0300

    mouse capture

--- Player.gd
+++ Player.gd
@@ -2,13 +2,18 @@ extends Camera
 
 export var speed = 2
 var velocity = Vector3(0,0,0)
+var esc_capture = false
+var rotating = true
 
 func _ready():
 	self.make_current()
+	print(get_viewport().size)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _process(delta):
 	var orientation = Quat(Vector3(0,1,0), self.rotation.project(Vector3(0,1,0)).angle_to(Vector3(0,1,0)))
 	self.translate(orientation.xform(velocity.normalized())*speed*delta)
+	print(esc_capture)
 	
 func _unhandled_input(event):
 	if event is InputEventKey:
@@ -20,3 +25,10 @@ func _unhandled_input(event):
 			velocity.x = int(event.pressed) * -1
 		elif event.scancode == KEY_A:
 			velocity.x = int(event.pressed) * 1
+		elif event.scancode == KEY_ESCAPE and event.pressed != esc_capture:
+			esc_capture = event.pressed
+			if event.pressed:
+				rotating = not rotating
+				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if rotating else Input.MOUSE_MODE_VISIBLE)
+	elif event is InputEventMouseMotion:
+		pass
