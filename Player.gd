6cadf2a7f475384c50a7deab0644263edd606f09
Gedas <g.simaitis@gmail.com>
2019-10-05 16:31:24 +0300

    somewhat fix movement

--- Player.gd
+++ Player.gd
@@ -1,34 +1,39 @@
 extends Camera
 
 export var speed = 2
+export var sensitivity = 1
 var velocity = Vector3(0,0,0)
 var esc_capture = false
 var rotating = true
+var mouse_ratio = 0
 
 func _ready():
 	self.make_current()
-	print(get_viewport().size)
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	self.recalculate_ratios()
 
 func _process(delta):
-	var orientation = Quat(Vector3(0,1,0), self.rotation.project(Vector3(0,1,0)).angle_to(Vector3(0,1,0)))
-	self.translate(orientation.xform(velocity.normalized())*speed*delta)
-	print(esc_capture)
-	
+	self.translate(velocity*speed*delta)
+	self.translation.y = 0
+
 func _unhandled_input(event):
 	if event is InputEventKey:
 		if event.scancode == KEY_S:
-			velocity.z = int(event.pressed) * -1
-		elif event.scancode == KEY_W:
 			velocity.z = int(event.pressed) * 1
+		elif event.scancode == KEY_W:
+			velocity.z = int(event.pressed) * -1
 		elif event.scancode == KEY_D:
-			velocity.x = int(event.pressed) * -1
-		elif event.scancode == KEY_A:
 			velocity.x = int(event.pressed) * 1
+		elif event.scancode == KEY_A:
+			velocity.x = int(event.pressed) * -1
 		elif event.scancode == KEY_ESCAPE and event.pressed != esc_capture:
 			esc_capture = event.pressed
 			if event.pressed:
 				rotating = not rotating
 				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if rotating else Input.MOUSE_MODE_VISIBLE)
-	elif event is InputEventMouseMotion:
-		pass
+	elif event is InputEventMouseMotion and rotating:
+		self.rotate(Vector3(0,1,0), event.relative.x * mouse_ratio)
+		self.rotate(self.transform.xform(Vector3(1,0,0)) - self.transform.origin, event.relative.y * mouse_ratio)
+
+func recalculate_ratios():
+	mouse_ratio = -deg2rad(self.fov) * sensitivity / get_viewport().size[0]
