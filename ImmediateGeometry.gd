2c190e3c6e1e24000573eeef20e1e228301e8ba4
Gedas <g.simaitis@gmail.com>
2019-10-05 14:09:05 +0300

    remove boilerplate comments

--- ImmediateGeometry.gd
+++ ImmediateGeometry.gd
@@ -1,10 +1,5 @@
 extends ImmediateGeometry
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	self.begin(Mesh.PRIMITIVE_TRIANGLE_STRIP)
 	self.add_vertex(Vector3(1,0,0))
@@ -15,8 +10,3 @@ func _ready():
 	self.add_vertex(Vector3(-1,0,0))
 	self.add_vertex(Vector3(1,0,0))
 	self.end()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
