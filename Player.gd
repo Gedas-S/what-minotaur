b9ddd1dc778f9c30e0eb8523f54b00f6fc61021d
Gedas <g.simaitis@gmail.com>
2019-10-05 15:05:46 +0300

    actually fix movement

--- Player.gd
+++ Player.gd
@@ -8,12 +8,6 @@ func _ready():
 
 func _process(delta):
 	var orientation = Quat(Vector3(0,1,0), self.rotation.project(Vector3(0,1,0)).angle_to(Vector3(0,1,0)))
-	print(rotation)
-	print(self.rotation.project(Vector3(0,1,0)))
-	print(self.rotation.project(Vector3(0,1,0)).angle_to(Vector3(1,0,0)))
-	print(Vector3(2,0,1).angle_to(Vector3(1,0,0)))
-	print(orientation)
-	print(orientation.xform(velocity.normalized()))
 	self.translate(orientation.xform(velocity.normalized())*speed*delta)
 	
 func _unhandled_input(event):
