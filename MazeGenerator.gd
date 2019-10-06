d1c0a78d28c941a2914c61ad5ddc8073eadfb653
Aivaras <jakaiv@gmail.com>
2019-10-06 12:49:41 +0300

    added wall placement method. run MazeGenerator scene to see it in action

--- MazeGenerator.gd
+++ MazeGenerator.gd
@@ -9,57 +9,31 @@ export (PackedScene) var WallSegment
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var mg = load("res://maze_generator.gd").new()
-	var maze = mg.prim(3,3)
+	var maze = mg.prim(10,10)
 	for x in range(maze.maxWidth):
 		print(maze.underlying[x])
 			
 	
-	grid(4, 4)
+	makeMaze(maze.underlying, 10)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-func rowOfWalls(width: int, offsetVec: Vector3):
-	var offsetX = width / 2
-	var scaleX = 10
-	for i in range(width):
-		var position = Vector3((i - offsetX)*scaleX, 0, 0)
-		var q = WallSegment.instance()
-		q.translate(position + offsetVec)
-		add_child(q)
-
-func grid(width=10, height=10):
-	var scaleX = 10
-	var scaleY = 10
-	
-	var shiftX = Vector3(scaleX / 2, 0, 0)
-	var shiftY = Vector3(0, 0, scaleY / 2)
-	
-	for i in range(width):
-		for j in range(height):
+# takes the underlying array[array] of PrimsGrid
+func makeMaze(grid: Array, wallSize: int):
+	var stepSize = wallSize / 2
+	for x in range(grid.size()):
+		var xOdd = x % 2 == 1
+		for y in range(grid[x].size()):
+			var yOdd = y % 2 == 1
 			
-			var position = Vector3(i*scaleX, 0, j*scaleY)
-			
-			var q = WallSegment.instance()
-			q.translate(position - shiftX)
-			
-			var qq = WallSegment.instance()
-			qq.translate(position - shiftY)
-			qq.rotate(Vector3(0, 1, 0), deg2rad(90))
-			
-			add_child(q)
-			add_child(qq)
-			
-		var posX = Vector3(i*scaleX, 0, 0) - 2*shiftY
-		var q = WallSegment.instance()
-		q.translate(posX - shiftX)
-		add_child(q)
-		
-		
-	for j in range(height):
-		var pos = Vector3(0, 0, j*scaleY) - 2*shiftX
-		var q = WallSegment.instance()
-		q.translate(pos - shiftY)
-		q.rotate(Vector3(0, 1, 0), deg2rad(90))
-		add_child(q)
+			if xOdd && !yOdd && grid[x][y] == 1:
+				var q = WallSegment.instance()
+				q.translate(Vector3(x * stepSize, 0, y*stepSize))
+				add_child(q)
+			elif !xOdd && yOdd && grid[x][y] == 1:
+				var q = WallSegment.instance()
+				q.translate(Vector3(x * stepSize, 0, y*stepSize))
+				q.rotate(Vector3(0, 1, 0), deg2rad(90))
+				add_child(q)
