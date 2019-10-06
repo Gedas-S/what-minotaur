b8b2bc58865f6ab787614cc4a6a40c19251cff94
Gedas <g.simaitis@gmail.com>
2019-10-06 15:31:20 +0300

    rolling segment

--- mobs/Segment.gd
+++ mobs/Segment.gd
@@ -3,9 +3,9 @@ extends RigidBody
 var follow_distance : float = 0
 
 func _ready():
-	self.follow_distance = self.get_node("CollisionShape").shape.radius + self.get_node("../CollisionShape").shape.radius
+	self.follow_distance = self.get_node("CollisionShape").shape.radius + self.get_node("../Worm/CollisionShape").shape.radius
 
 func _process(delta):
-	var direction : Vector3 = self.get_parent().global_transform.origin - self.global_transform.origin
-	var vector = direction * (direction.length() - self.follow_distance)
+	var direction : Vector3 = self.get_parent().get_node("Worm").global_transform.origin - self.global_transform.origin
+	var vector = direction * 5
 	self.add_central_force(vector)
