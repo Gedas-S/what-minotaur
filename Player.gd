a134b81c1b9e315a1cacad57ca179acab1023be9
Gedas <g.simaitis@gmail.com>
2019-10-06 09:49:17 +0300

    remove old hammer swing attempt

--- Player.gd
+++ Player.gd
@@ -1,36 +1,23 @@
 extends KinematicBody
 
-export var speed = 150
-export var sensitivity = 1
-var velocity = Vector3(0,0,0)
-var esc_capture = false
-var rotating = true
-var mouse_ratio = 0
-var camera = Camera
-
-var hammer = Spatial
-var hammer_phase = 5
-var hammer_origin = Vector3(0,0,0)
-var hammer_rotation = Vector3(0,0,0)
-var hammer_last_phase = 0
-var hammer_from_loc = Vector3(0,0,0)
-var hammer_from_rot = Vector3(0,0,0)
+export var speed : float = 150
+export var sensitivity : float = 1
+var velocity : Vector3 = Vector3(0,0,0)
+var esc_capture : bool = false
+var rotating : bool = true
+var mouse_ratio : float = 0
+var camera : Camera
+var hammer : AnimationPlayer
 
 func _ready():
 	self.camera = self.get_node("PlayerEyes")
 	self.camera.make_current()
-	self.hammer = self.get_node("Hammer/Handle")
-	self.hammer_origin = self.hammer.transform.origin
-	self.hammer_rotation = self.hammer.transform.basis
+	self.hammer = self.get_node("Hammer/AnimationPlayer")
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	self.recalculate_ratios()
-	self.translate(Vector3(0,0,10))
 
 func _process(delta):
 	self.move_and_slide((self.transform.xform(velocity.normalized())-self.transform.origin)*speed*delta)
-	if hammer_phase < 3:
-		hammer_phase += delta
-		self.do_hammering(hammer_phase)
 
 func _unhandled_input(event):
 	if event is InputEventKey:
@@ -43,9 +30,7 @@ func _unhandled_input(event):
 		elif event.scancode == KEY_A:
 			velocity.x = int(event.pressed) * -1
 		elif event.scancode == KEY_E:
-			if hammer_phase >= 3:
-				hammer_phase = 0
-			self.get_parent().get_node("WallSegment2").shatter()
+			self.hammer.play("SwingHammer")
 		elif event.scancode == KEY_ESCAPE and event.pressed != esc_capture:
 			esc_capture = event.pressed
 			if event.pressed:
@@ -57,11 +42,3 @@ func _unhandled_input(event):
 
 func recalculate_ratios():
 	mouse_ratio = -deg2rad(self.camera.fov) * sensitivity / get_viewport().size[0]
-	
-func do_hammering(phase):
-	if phase < 1:
-		if hammer_last_phase != 1:
-			hammer_last_phase = 1
-			hammer_from_loc = hammer.transform.origin
-			hammer_from_rot = hammer.transform.rotation
-		hammer.transform.origin = hammer_from_loc.linear_interpolate(Vector3(0,2,0))
